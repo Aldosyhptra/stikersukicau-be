22803f5438f732a41340691529b97f1f5030fe7e
Aldo Syahputra <aldosyahputra80@gmail.com>
2025-06-20 02:55:45 +0700

    first commit dan membuat api uploadgambar

---
+++ app/Http/Controllers/GambarController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gambar;
+use Illuminate\Http\Request;
+
+class GambarController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'gambar' => 'nullable|image|mimes:jpg,jpeg,png,gif|max:1024',
+            'kategori' => 'nullable|string|max:255',
+        ]);
+
+        $path = null;
+
+        if ($request->hasFile('gambar')) {
+            $file = $request->file('gambar');
+            $filename = time() . '_' . $file->getClientOriginalName(); 
+            $folderPath = public_path('gambar'); 
+
+            if (!file_exists($folderPath)) {
+                mkdir($folderPath, 0777, true);
+            }
+
+            $file->move($folderPath, $filename);
+        }
+
+        $gambar = Gambar::create([
+            'gambar' => $filename,
+            'kategori' => $request->kategori,
+        ]);
+
+        return response()->json([
+            'message' => 'Gambar berhasil disimpan',
+            'data' => $gambar
+        ], 201);
+    }
+}
+
