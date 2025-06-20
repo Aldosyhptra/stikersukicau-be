2f6bf936ef825e26733f9009e87a3c9abdf5089d
Aldo Syahputra <aldosyahputra80@gmail.com>
2025-06-21 04:45:47 +0700

    atur config

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GambarController;
 
 Route::post('/uploadgambar', [GambarController::class, 'store']);
+Route::get('/read_all', [GambarController::class, 'fetchAll']);
